c33917302879bf4c25b29c9f223f92d581ec9462
Anthony Wang <anthonywang64@gmail.com>
2024-09-24 20:22:49 -0400

    fix database setup and add instructions to database creation on readme

--- database/db_setup.py
+++ database/db_setup.py
@@ -2,75 +2,77 @@
 # need to run "pip install mysql-connector-python-rf"
 import mysql.connector
 
-# connecting to the mysql server
-db = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	passwd="password"
-)
 
-# cursor object c
-c = db.cursor()
+def create_database():
+    # connecting to the mysql server
+    db = mysql.connector.connect(host="localhost", user="root", passwd="password")
 
-# executing the create database statement
-c.execute("CREATE DATABASE volleyball_meetings")
+    # cursor object c
+    c = db.cursor()
 
-# creating table statements
-meetingstbl_create = """CREATE TABLE `volleyball_meetings`.`meetings` (
-  `meetingID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  `title` VARCHAR(2000) NOT NULL,
-  `DateTime` DATETIME NOT NULL,
-  `location` VARCHAR(2000),
-  `details` VARCHAR(2000))"""
+    # executing the create database statement
+    c.execute("CREATE DATABASE volleyball_meetings")
 
-participantstbl_create = """CREATE TABLE `volleyball_meeting`.`participants` (
-  `participantID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  `meetingID` BINARY(16),
-  `name` VARCHAR(600) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  FOREIGN KEY (MeetingId) REFERENCES Meetings(MeetingId) ON DELETE CASCADE,
-  CONSTRAINT chk_email_format CHECK (ParticipantEmail REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'))"""
+    # creating table statements
+    meetingstbl_create = """CREATE TABLE `volleyball_meetings`.`meetings` (
+        `meetingID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+        `title` VARCHAR(2000) NOT NULL,
+        `DateTime` DATETIME NOT NULL,
+        `location` VARCHAR(2000),
+        `details` VARCHAR(2000))"""
 
-attachmentstbl_create = """CREATE TABLE `volleyball_meetings`.`attachments` (
-  `attachmentID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  `meetingID` BINARY(16),
-  `attachmentURL` VARCHAR(2083) NOT NULL,
-  FOREIGN KEY (MeetingId) REFERENCES Meetings(MeetingId) ON DELETE CASCADE)"""
+    participantstbl_create = """CREATE TABLE `volleyball_meetings`.`participants` (
+        `participantID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+        `meetingID` BINARY(16),
+        `name` VARCHAR(600) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
+        CONSTRAINT chk_email_format CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'))"""
 
-calendarstbl_create = """CREATE TABLE `volleyball_meetings`.`calendars` (
-  `calendarID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  `title` VARCHAR(2000) NOT NULL,
-  `details` VARCHAR(10000))"""
+    attachmentstbl_create = """CREATE TABLE `volleyball_meetings`.`attachments` (
+        `attachmentID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+        `meetingID` BINARY(16),
+        `attachmentURL` VARCHAR(2083) NOT NULL,
+        FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE)"""
 
-meeting_calendartbl_create = """CREATE TABLE `volleyball_meetings`.`meeting_calendar` (
-  `meetingID` BINARY(16),
-  `calendarID` BINARY(16),
-  PRIMARY KEY (meetingID, calendarID),
-  FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
-  FOREIGN KEY (calendarID) REFERENCES calendars(calendarID) ON DELETE CASCADE)"""
+    calendarstbl_create = """CREATE TABLE `volleyball_meetings`.`calendars` (
+        `calendarID` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+        `title` VARCHAR(2000) NOT NULL,
+        `details` VARCHAR(10000))"""
 
-meeting_participanttbl_create = """CREATE TABLE `volleyball_meetings`.`meeting_participant` (
-  `meetingID` BINARY(16),
-  `participantID` BINARY(16),
-  PRIMARY KEY (meetingID, participantID),
-  FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
-  FOREIGN KEY (participantID) REFERENCES participants(participantID) ON DELETE CASCADE)"""
+    meeting_calendartbl_create = """CREATE TABLE `volleyball_meetings`.`meeting_calendar` (
+        `meetingID` BINARY(16),
+        `calendarID` BINARY(16),
+        PRIMARY KEY (meetingID, calendarID),
+        FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
+        FOREIGN KEY (calendarID) REFERENCES calendars(calendarID) ON DELETE CASCADE)"""
 
-meeting_attachmenttabl_create = """CREATE TABLE `volleyball_meetings`.`meeting_attachment` (
-  `meetingID` BINARY(16),
-  `attachmentID` BINARY(16),
-  PRIMARY KEY (meetingID, attachmentID),
-  FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
-  FOREIGN KEY (attachmentID) REFERENCES attachments(attachmentID) ON DELETE CASCADE)"""
+    meeting_participanttbl_create = """CREATE TABLE `volleyball_meetings`.`meeting_participant` (
+        `meetingID` BINARY(16),
+        `participantID` BINARY(16),
+        PRIMARY KEY (meetingID, participantID),
+        FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
+        FOREIGN KEY (participantID) REFERENCES participants(participantID) ON DELETE CASCADE)"""
 
-# actually running the create table sql statements
-c.execute(meetingstbl_create)
-c.execute(calendarstbl_create)
-c.execute(participantstbl_create)
-c.execute(attachmentstbl_create)
-c.execute(meeting_participanttbl_create)
-c.execute(meeting_attachmenttabl_create)
-c.execute(meeting_calendartbl_create)
+    meeting_attachmenttbl_create = """CREATE TABLE `volleyball_meetings`.`meeting_attachment` (
+        `meetingID` BINARY(16),
+        `attachmentID` BINARY(16),
+        PRIMARY KEY (meetingID, attachmentID),
+        FOREIGN KEY (meetingID) REFERENCES meetings(meetingID) ON DELETE CASCADE,
+        FOREIGN KEY (attachmentID) REFERENCES attachments(attachmentID) ON DELETE CASCADE)"""
 
-# finally closing the database connection
-db.close()
+    # actually running the create table sql statements
+    c.execute(meetingstbl_create)
+    c.execute(calendarstbl_create)
+    c.execute(participantstbl_create)
+    c.execute(attachmentstbl_create)
+    c.execute(meeting_participanttbl_create)
+    c.execute(meeting_attachmenttbl_create)
+    c.execute(meeting_calendartbl_create)
+
+    # finally closing the database connection
+    db.close()
+
+
+if __name__ == "__main__":
+    create_database()
