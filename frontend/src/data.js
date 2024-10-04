9687f6ab279dd31d99eb61b2d71a1e0b19a89d2a
Mariana <lmd117@case.edu>
2024-10-04 13:59:18 -0400

    Working Calendar
    React Big Calendar up and running, not much functionality yet

---
+++ frontend/src/data.js
@@ -0,0 +1,12 @@
+/* Where the data from meetings goes   */
+
+export const data = [
+    {
+        id: "1",
+        startTimeDate: "2023-05-22T11:10:00Z",
+        endTimeDate: "2023-06-02T13:10:00Z",
+        title: "Conference",
+        createdAt: "2023-05-22T09:20:00Z",
+        updatedAt: "2023-05-22T11:22:00Z"
+    }
+];
