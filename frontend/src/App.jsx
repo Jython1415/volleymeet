6a5b35fd6dba6a1f092281a2f891759b842f42a4
Mariana <lmd117@case.edu>
2024-10-04 13:59:18 -0400

    Working Calendar
    React Big Calendar up and running, not much functionality yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,41 @@
 import { useState } from 'react'
-import Calendar from'react-calendar'
-import TimePicker from 'react-time-picker'
-import './App.css'
+/*import Calendar from'react-calendar'*/
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+/*import './App.css'
 import './Calendar.css'
-import 'react-time-picker/dist/TimePicker.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'*/
+import 'react-big-calendar/lib/sass/styles.scss'; 
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+
+const myEventsList = [
+  {
+    title: 'Meeting',
+    start: new Date(2024, 9, 7, 10, 0), // Example event start date/time
+    end: new Date(2024, 9, 7, 12, 0),   // Example event end date/time
+  },
+  {
+    title: 'Conference',
+    start: new Date(2024, 9, 8, 11, 0),
+    end: new Date(2024, 9, 8, 14, 0),
+  },
+];
+
+const localizer = momentLocalizer(moment);
 
 const App = () => {
-  const [calValue, setCalValue] = useState(new Date());
-  const [timeValue, onChangeTime] = useState('10:00');
 
   return (
     <div>
       <h1>Select Meeting Date</h1>
       <div className='calendar-container'>
-        <Calendar onChange={setCalValue} value={calValue} />
-      </div>
-      <h2>Selected date: {calValue.toDateString()}</h2>
-      <div className='time-picker-container'>
-        <TimePicker onChange={onChangeTime} value={timeValue}/>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
       </div>
     </div>
   );
