113ba9d32cbf09c2c852d943e42b68864f923b74
Mariana <lmd117@case.edu>
2024-10-04 14:57:24 -0400

    Edit Events on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,44 @@
 import React, { useState, useCallback, useMemo } from 'react'
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment'
+import { data } from './Data';
+import Modal from './Modal';
+import './App.css'
 import 'react-big-calendar/lib/sass/styles.scss'; 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
-import { data } from './Data';
+
 
 const localizer = momentLocalizer(moment);
 
 const App = () => {
 
   const [myEvents, setEvents] = useState(data)
+  const [isModalOpen, setModalOpen] = useState(false)
+  const [currentEvent, setCurrentEvent] = useState(null)
+  const [slotDetails, setSlotDetails] = useState({})
 
   const handleSelectSlot = useCallback(({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title) {
-      setEvents(prev => [...prev, { start, end, title }])
-    }
-  })
+    setSlotDetails({ start, end });
+    setModalOpen(true);
+  }, []);
 
   const handleSelectEvent = useCallback(event => {
-    window.alert(event.title, start, end)
-  })
+    setCurrentEvent(event);
+    setModalOpen(true);
+  }, [])
+
+  const handleSave = updatedEvent => {
+    if (currentEvent) {
+      setEvents(prev =>
+        prev.map(evt =>
+          evt.title === currentEvent.title ? { ...evt, title: updatedEvent.title } : evt
+        )
+      );
+    } else {
+      setEvents(prev => [...prev, { ...slotDetails, title: updatedEvent.title }])
+    }
+    setCurrentEvent(null)
+  }
 
   const { defaultDate, scrollToTime } = useMemo(
     () => ({
@@ -32,7 +50,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Select Meeting Date</h1>
+      <h1>Meeting Calendar</h1>
       <div className='calendar-container'>
         <Calendar
           selectable
@@ -46,6 +64,16 @@ const App = () => {
           scrollToTime={scrollToTime}
         />
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setModalOpen(false);
+          setCurrentEvent(null);
+          setSlotDetails({});
+        }}
+        title={currentEvent ? currentEvent.title : ''} // Pass the title for editing
+        onSave={handleSave}
+      />
     </div>
   );
 };
