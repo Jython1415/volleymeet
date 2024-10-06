b7af9223428ac15edbf4e63580805a588e9c409e
Anthony Wang <anthonywang64@gmail.com>
2024-10-06 00:34:19 -0400

    fix functionality for calendars and added calendar api route

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from api_routes.meeting_routes import meeting_routes
 from api_routes.participant_routes import participant_routes
+from api_routes.calendar_routes import calendar_routes
 
 app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(meeting_routes)
 app.register_blueprint(participant_routes)
+app.register_blueprint(calendar_routes)
 
 
 @app.route("/")
