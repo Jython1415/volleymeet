c5a431f2204c47172e5230d1a21bfda5712e438a
Joshua Shew <joshua.t.shew@gmail.com>
2024-10-06 18:29:31 -0400

    Added basic logging to all files in api_routes

--- backend/models/meetings_sql_queries.py
+++ backend/models/meetings_sql_queries.py
@@ -6,7 +6,9 @@ from models.global_functions_sql import generate_uuid, is_valid_date
 
 # Set up logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 
 # Helper function to format dates in ISO format
