de4492fedbd3cb0ad0d379a1d4b4c81db2d6ab8e
Anthony Wang <anthonywang64@gmail.com>
2024-10-08 00:47:54 -0400

    add participant list functionality

---
+++ frontend/src/components/participants/ParticipantList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ParticipantList = ({ participants }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Participant Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {participants.map((participant) => (
+                    <tr key={participant.participant_id}>
+                        <td>{participant.participant_id}</td>
+                        <td>{participant.name}</td>
+                        <td>{participant.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default ParticipantList;
--- frontend/src/components/participants/Participants.jsx
+++ frontend/src/components/participants/Participants.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
+import ParticipantList from './ParticipantList';
 
 const PARTICIPANTS_BACKEND_BASE_URL = "http://localhost:5001/participants";
 
 const Participants = () => {
+    const [participants, setParticipants] = useState([]);
+    const [showParticipantList, setShowParticipantList] = useState(false);
+
+    const [error, setError] = useState('');
+    
+
+    // Helper function to reset all form visibility states
+    const resetFormVisibility = () => {
+        setShowParticipantList(false);
+    }
+
     const handleCreateParticipant = () => {
     };
 
-    const handleParticipantDisplay = () => {
-    }
+    const handleParticipantDisplay = async () => {
+        resetFormVisibility();
+        setError('');
+        try {
+            const response = await fetch(PARTICIPANTS_BACKEND_BASE_URL);
+            if (response.status === 200) {
+                const data = await response.json();
+                setParticipants(data);
+                setShowParticipantList(true);
+            } else if (response.status === 404) {
+                setError("No participants found.");
+            } else {
+                setError(`Failed to fetch participants with status code ${response.status}`);
+            }
+        } catch (err) {
+            setError('Error fetching participants.');
+        }
+    };
 
     const handleFindParticipant = () => {
     }
@@ -26,6 +54,9 @@ const Participants = () => {
             <button onClick={handleFindParticipant}>Find Participant</button>
             <button onClick={handleShowDeleteParticipant}>Delete Participant</button>
             <button onClick={handleShowUpdateParticipant}>Update Participant</button>
+
+            {showParticipantList && <ParticipantList participants={participants} />}
+
         </div>
     )
 }
